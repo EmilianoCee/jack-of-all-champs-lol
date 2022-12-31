006cc67704fa2251f2fb50b11e0668645a8df9e8
Emiliano C <emiliano.cereceres@student.cart.org>
2022-12-30 22:31:25 -0800

    sort by role
    every champion has their roles added in HTML. Created function that sorts roles by putting in role into search bar.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,5 @@
+var toggle = false;
+
 function searchChamp() {
     let input = document.getElementById("search").value
     input = input.toLowerCase();
@@ -14,25 +16,39 @@ function searchChamp() {
     }
 }
 
+// function sortRole() {
+//     let input = document.getElementById("search").value
+//     input = input.toLowerCase();
+//     let x = document.getElementsByClassName("role");
 
-function sortRole() {
-    let input = document.getElementById("top").innerText
-    input = document.getElementById("search").value;
+//     if (toggle === true ) {
+//         document.getElementById(clickedId).style.opacity = "100%"
+//     } else {
+//         document.getElementById(clickedId).style.opacity = "25%"
+//     }
+//     toggle = !toggle;
+// }
+
+function sortRole(clickedId) {
+    let input = document.getElementById("search").value;
+    let x = document.getElementById(clickedId);
+    document.getElementById("search").value = x.innerText;
+    searchChamp();
 }
+
 
 
 // Toggle for champ icons
-function gatherId(clickedId) {
-    changeop(clickedId)
-}
+// function gatherId(clickedId) {
+//     changeop(clickedId)
+// }
 
-var toggle = false;
 
 function changeop(clickedId) {
     if (toggle === true ) {
-        document.getElementById(clickedId).style.opacity = "100%"
+        document.getElementById(clickedId).style.opacity = "100%";
     } else {
-        document.getElementById(clickedId).style.opacity = "25%"
+        document.getElementById(clickedId).style.opacity = "25%";
     }
     toggle = !toggle;
 }
@@ -63,16 +79,21 @@ function toggleMenu() {
     const menu = document.querySelector(".menu");
 
     if (menu.classList.contains("show-menu")) {
-        menu.classList.remove("show-menu")
+        menu.classList.remove("show-menu");
         document.querySelector(".close-icon").style.display = "none";
         document.querySelector(".burger-icon").style.display = "block";
         menu.style.display = "none";
         document.styleSheets[0].cssRules[5].style.setProperty("display", "flex");
     } else {
-        menu.classList.add("show-menu")
+        menu.classList.add("show-menu");
         document.querySelector(".close-icon").style.display = "block";
         document.querySelector(".burger-icon").style.display = "none";
         menu.style.display = "flex";
         document.styleSheets[0].cssRules[5].style.setProperty("display", "none");
     }
 }
+
+
+// fetch("http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json")
+//     .then(res => res.json())
+//     .then(data => console.log(data))
